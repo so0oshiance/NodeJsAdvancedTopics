b0a3485aeb4062b741d252b4ab231da72e5994e4
so0oshiance <m.farhani@rightel.ir>
2019-07-04 16:33:22 +0430

    setup.js for configutating oue jest

--- tests/Factories/userFactory.js
+++ tests/Factories/userFactory.js
@@ -5,6 +5,7 @@ module.exports=()=>{
     /**
      * new User({}).save() would return promis and is a asyc task so we can await for that
      * where we wanna use
+     * 
      */
     return new User({}).save(); 
 }
--- tests/setup.js
+++ tests/setup.js
@@ -2,6 +2,14 @@ require('../models/User');
 
 const mongoose=require('mongoose');
 const keys=require('../config/keys');
-
+//mongoose by defaulf wont use promise, we should tell it to use it so
 mongoose.Promise=global.Promise;
-mongoose.connect(keys.mongoURI,{useMongoClient:true})
+mongoose.connect(keys.mongoURI,{useMongoClient:true});
+
+/**
+ * to tell JEST that before runnig test please execute our setup file first ! 
+ * we should do sonthing in our package.json file. tag below is added in package.json:
+ * "jest":{
+    "setupTestFrameworkScriptFile":"./tests/setup.js"
+  },
+ */
